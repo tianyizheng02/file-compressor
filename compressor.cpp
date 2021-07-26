104f30e8512d9a6b5d1580c691c5ca3c9cc561cb
Tianyi Zheng <tianyizheng02@gmail.com>
2021-07-26 01:43:30 -0700

    Add handling of command line arguments

--- compressor.cpp
+++ compressor.cpp
@@ -1,7 +1,135 @@
 #include <iostream>
+#include <fstream>
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+// ANSI color codes
+const char *RESET = "\033[0m";
+const char *BOLD = "\033[1m";
+const char *UL = "\033[4m";
+const char *BOLD_RED = "\033[1;31m";
+
+/**
+ * Display help menu
+ *
+ * @param program Program name (argv[0])
+ */
+void show_help(const std::string &program) {
+    std::cout << BOLD << "USAGE: " << program << RESET
+              << " [" << UL << "option" << RESET << "] "
+              << UL << "file-name" << RESET << "\n" << std::endl;
+
+    std::cout << std::left << std::setw(4) << "-d"
+              << std::setw(16) << "--decompress"
+              << "Decompress a file" << "\n"
+              << std::setw(4) << "-h"
+              << std::setw(16) << "--help"
+              << "Open this help menu (requires no file name)" << "\n"
+              << std::setw(4) << "-r"
+              << std::setw(16) << "--reset"
+              << "Reset the keys dictionary during compression" << std::endl;
+}
+
+/**
+ * Get file if it exists
+ *
+ * @param name File name
+ * @return input stream for file
+ */
+std::ifstream find_file(const std::string &name) {
+    std::ifstream file(name);
+    if (!file.is_open()) {
+        std::cout << BOLD_RED << "ERROR: file " << name
+                  << " not found or could not be opened" << RESET << std::endl;
+        file.close();
+        exit(1);
+    }
+
+    return file;
+}
+
+/**
+ * Compress file using a modified LZW compression algorithm
+ *
+ * @param name File name
+ * @param reset Whether to reset LZW keys dictionary
+ */
+void compress(const std::string &name, const bool reset) {
+    std::ifstream file = find_file(name);
+
+    // TODO: Implement LZW compress
+
+    file.close();
+}
+
+/**
+ * Decompress file using a modified LZW decompression algorithm
+ *
+ * @param name File name
+ */
+void decompress(const std::string &name) {
+    std::ifstream file = find_file(name);
+
+    // TODO: Implement LZW decompress
+
+    file.close();
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << BOLD_RED << "ERROR: No arguments detected" << "\n" << RESET
+                  << std::endl;
+        show_help(argv[0]);
+        return 1;
+    }
+
+    bool comp = true;   // Compress or decompress
+    bool reset = false; // Reset LZW keys dictionary or not
+
+    opterr = 0; // Don't output default error messages for invalid options
+    struct option options[] = {
+            {"decompress", no_argument, nullptr, 'd'},
+            {"help",       no_argument, nullptr, 'h'},
+            {"reset",      no_argument, nullptr, 'r'},
+            {nullptr,      no_argument, nullptr, 0}
+    };
+
+    switch (getopt_long(argc, argv, "dhr", options, nullptr)) {
+        case -1:    // "argv[0] ..."
+            break;
+        case 'd':   // "argv[0] -d ..."
+            comp = false;
+            break;
+        case 'r':   // "argv[0] -r ..."
+            reset = true;
+            break;
+        case 'h':   // "argv[0] -h ..."
+            show_help(argv[0]);
+            return 0;
+        default:
+            std::cout << BOLD_RED << "ERROR: Unknown option detected" << RESET
+                      << "\n" << std::endl;
+            show_help(argv[0]);
+            return 1;
+    }
+
+    // Multiple options
+    if (getopt_long(argc, argv, "dhr", options, nullptr) != -1) {
+        std::cout << BOLD_RED << "ERROR: multiple options detected" << RESET
+                  << "\n" << std::endl;
+        show_help(argv[0]);
+        return 1;
+    }
+
+    // Option -d or -r with no file name
+    if (optind == argc) {
+        std::cout << BOLD_RED << "ERROR: No file detected" << RESET << "\n"
+                  << std::endl;
+        show_help(argv[0]);
+        return 1;
+    }
+
+    if (comp) while (optind < argc) compress(argv[optind++], reset);
+    else while (optind < argc) decompress(argv[optind++]);
 
     return 0;
 }
